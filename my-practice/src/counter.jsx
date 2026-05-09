ed7081cf7212e6a39874ebb29a3efb3752df4514
BOOBESH776 <cboobesh776@gmail.com>
2026-05-09 15:59:43 +0530

    basic one

---
+++ my-practice/src/counter.jsx
@@ -0,0 +1,8 @@
+const A = () => {
+    return (
+        <div>
+            <h1>Counter</h1>
+        </div>
+    );
+}
+export default A;
--- my-practice/src/main.jsx
+++ my-practice/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import A from './counter';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App = () => {
+    return (
+        <div>
+            <A />
+        </div>
+    );
+}
+
+export default App;
