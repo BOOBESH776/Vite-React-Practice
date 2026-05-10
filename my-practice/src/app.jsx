fa05ad6f11716eaf1d3cc191b9bdbefdd854d39a
BOOBESH776 <cboobesh776@gmail.com>
2026-05-10 19:27:13 +0530

    import react component from react lib

--- my-practice/src/app.jsx
+++ my-practice/src/app.jsx
@@ -1,13 +1,15 @@
 // import A from './counter';
 // import './index.css';
+import React from 'react';
 import './components/style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+    let user = 'CM';
     return (
         <>
-        <Header user = "CM"/>
+        <Header user={user} />
         <Footer/>
         </>
     );
--- my-practice/src/components/Header.jsx
+++ my-practice/src/components/Header.jsx
@@ -1,9 +1,11 @@
-
-function Header(){
+import React from 'react';
+function Header(props){
+    console.log(props);
         return(
         <header>
             <>
                 <h1 style={{ color: 'black' }}>Header</h1>
+                <p>Hello, {props.user}!</p>
             </>
         </header>
     );
