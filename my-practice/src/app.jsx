708ac6b913c8b536ab18435e9933998fcc357633
BOOBESH776 <cboobesh776@gmail.com>
2026-05-11 12:23:32 +0530

    install style components in and have some button style

--- my-practice/src/app.jsx
+++ my-practice/src/app.jsx
@@ -4,12 +4,14 @@ import React from 'react';
 import './components/style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Content from './components/Content';
 
 function App() {
     let user = 'CM';
     return (
         <>
         <Header user={user} />
+        <Content />
         <Footer/>
         </>
     );
