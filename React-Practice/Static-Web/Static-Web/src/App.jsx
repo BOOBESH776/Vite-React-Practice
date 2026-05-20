48c0e173ee9e7d47af526e0127e12b899f6c2383
BOOBESH776 <cboobesh776@gmail.com>
2026-05-20 11:53:48 +0530

    nav and text color

--- React-Practice/Static-Web/Static-Web/src/App.jsx
+++ React-Practice/Static-Web/Static-Web/src/App.jsx
@@ -1,9 +1,11 @@
+import Color from "./Components/color";
 import Nav from "./Components/nav";
 
 function App() {
   return (
     <>
     <Nav />
+    <Color />
     </>
   );
 }
---
+++ React-Practice/Static-Web/Static-Web/src/Components/Color.jsx
@@ -0,0 +1,9 @@
+const Color = () => {
+    return (
+        <div className="color">
+            <h1 style={{ color: 'white' }}>Color</h1>
+        </div>
+    );
+}
+
+export default Color;
